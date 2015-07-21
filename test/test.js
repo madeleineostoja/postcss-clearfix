8b5618cf3aeecfc001a0729af146be3d800022e3
Sean King <sean@seanking.org>
2015-07-21 10:39:53 +1000

    refactor tests into fixutres

--- test/test.js
+++ test/test.js
@@ -3,33 +3,42 @@
 
 var postcss = require('postcss'),
     expect = require('chai').expect,
+    fs = require('fs'),
+    path = require('path'),
     plugin = require('../');
 
-var test = function (input, output, opts, done) {
-  postcss([ plugin(opts) ]).process(input).then(function (result) {
-    expect(result.css).to.eql(output);
-    expect(result.warnings()).to.be.empty;
+var test = function (fixture, opts, done) {
+  var input = fixture + '.css',
+      expected = fixture + '.expected.css';
+
+  input = fs.readFileSync(path.join(__dirname, 'fixtures', input), 'utf8');
+  expected = fs.readFileSync(path.join(__dirname, 'fixtures', expected), 'utf8');
+
+  postcss([ plugin(opts) ])
+    .process(input)
+    .then(function (result) {
+      expect(result.css).to.eql(expected);
+      console.log(result.warnings());
+      expect(result.warnings()).to.be.empty;
     done();
   }).catch(function (error) {
     done(error);
   });
+
 };
 
 describe('postcss-clearfix', function () {
 
-  it('sets clear:fix correctly', function (done) {
-   test('a{ clear: fix; }',
-        'a:after {\n    content: \'\';\n    display: table;\n    clear: both\n}', { }, done);
+  it('sets clear:fix correctly', function(done) {
+   test('fix', {}, done);
   });
 
-  it('sets clear:fix-legacy correctly', function (done) {
-    test('a{ clear: fix-legacy; }',
-         'a:before,\na:after{ content: \'\'; display: table; }\na:after{ clear: both; }\na{ zoom: 1; }', { }, done);
+  it('sets clear:fix-legacy correctly', function(done) {
+    test('fix-legacy', {}, done);
   });
 
-  it('does\'t destroy the node if there\'s more than one declaration in it', function (done) {
-    test('a{ clear: fix; color: #fff; }',
-         'a:after{ content: \'\'; display: table; clear: both; }\na{ color: #fff; }', { }, done);
+  it('does\'t destroy the node if there\'s more than one declaration in it', function(done) {
+    test('multiple', {}, done);
   });
 
 });
