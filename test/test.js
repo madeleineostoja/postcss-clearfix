5f9fc796642d123ac2d20e2c96e9bb377a15b452
Bogdan Chadkin <trysound@yandex.ru>
2016-02-14 11:50:22 +0300

    Improve tests

--- test/test.js
+++ test/test.js
@@ -1,4 +1,3 @@
-/*eslint no-unused-expressions: 0*/
 'use strict';
 
 var postcss = require('postcss'),
@@ -7,38 +6,44 @@ var postcss = require('postcss'),
     path = require('path'),
     plugin = require('../');
 
-function test(fixture, opts, done) {
-  var input = fixture + '.css',
-      expected = fixture + '.expected.css';
+process.chdir('test');
 
-  input = fs.readFileSync(path.join(__dirname, 'fixtures', input), 'utf8');
-  expected = fs.readFileSync(path.join(__dirname, 'fixtures', expected), 'utf8');
+function readFixture(file) {
+  file = path.join('fixtures', file);
+  file = fs.readFileSync(file, 'utf8');
+  // Fix windows linebreaks
+  return file.replace(/\r\n/g, '\n');
+}
 
-  postcss([ plugin(opts) ])
+function test(fixture, opts) {
+  var input = readFixture(fixture + '.css'),
+      expected = readFixture(fixture + '.expected.css');
+
+  return postcss([ plugin(opts) ])
     .process(input)
     .then(function (result) {
       expect(result.css).to.eql(expected);
-      console.log(result.warnings());
-      expect(result.warnings()).to.be.empty;
-    done();
-  }).catch(function (error) {
-    done(error);
+      var warnings = result.warnings();
+      if (warnings.length) {
+        console.log(warnings);
+      }
+      expect(warnings).to.be.empty;
   });
 
 };
 
 describe('postcss-clearfix', function () {
 
-  it('sets clear:fix correctly', function(done) {
-   test('fix', {}, done);
+  it('sets clear:fix correctly', function() {
+    return test('fix', {});
   });
 
-  it('sets clear:fix-legacy correctly', function(done) {
-    test('fix-legacy', {}, done);
+  it('sets clear:fix-legacy correctly', function() {
+    return test('fix-legacy', {});
   });
 
-  it('handles multiple selectors and declerations', function(done) {
-    test('multiple', {}, done);
+  it('handles multiple selectors and declerations', function() {
+    return test('multiple', {});
   });
 
 });
